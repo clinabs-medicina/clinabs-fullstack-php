73ac66fe9d37e95072e8ae28cd1393dcb3ec0cf9
Adriano Neco <desenvolvedor@clinabs.com>
2025-01-17 13:54:25 -0300

    fix:agenda

--- api/google_calendar/index.php
+++ api/google_calendar/index.php
@@ -1,19 +1,24 @@
 <?php
-require_once './googleCalendar.php';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-if(isset($_GET['token'])) {
-  $medico = " AND token = '{$_GET['token']}'";
+require_once './googleCalendar.php';
+$eventsArray = [];
+
+if (isset($_REQUEST['token'])) {
+  $condicao = "token = '{$_REQUEST['token']}'";
 } else {
-  $medico = "";
+  $condicao = "google_agenda_link LIKE 'https://calendar.google.com/calendar/ical/%'";
 }
 
 date_default_timezone_set('America/Sao_Paulo');
+
 $headers = ['Authorization' => 'all'];
 
-$servername = '68.183.159.246';
-$database = 'clinabs_app';
-$username = 'clinabs_admin';
-$password = 'GenP+s+J6Cisa^vB7visr@%3c0nCaOz#3Bb7jaGJ6pyOqC*C';
+$servername = '64.225.0.218';
+$database = 'clinabs_homolog';
+$username = 'clinabs_dev';
+$password = '&?7z?Yw$0]62N!gbn=l_@bbA0O{TRg:s';
 
 date_default_timezone_set('America/Sao_Paulo');
 
@@ -24,81 +29,82 @@ try {
   $pdo = new PDO("mysql:host=$servername;dbname=$database;charset=utf8mb4", $username, $password);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-  echo "Connection Failed: " . $e->getMessage();
+  echo 'Connection Failed: ' . $e->getMessage();
 }
 
-
-function isTimeInRange($checkTime, $startTime, $endTime) {
-    return strtotime($checkTime) >= strtotime($startTime) && strtotime($checkTime) <= strtotime($endTime);
+function isTimeInRange($checkTime, $startTime, $endTime)
+{
+  return strtotime($checkTime) >= strtotime($startTime) && strtotime($checkTime) <= strtotime($endTime);
 }
 
-    
-    $stmt = $pdo->query("SELECT inicio_ag,fim_ag,google_agenda_link,duracao_atendimento,token,(SELECT calendario FROM AGENDA_MEDICA WHERE medico_token = token) AS calendario FROM MEDICOS WHERE google_agenda_link != '' {$medico}");
+$stmt = $pdo->query("SELECT inicio_ag,fim_ag,google_agenda_link,duracao_atendimento,token,(SELECT calendario FROM AGENDA_MEDICA WHERE medico_token = token) AS calendario FROM MEDICOS WHERE {$condicao}");
 
-    if($stmt->rowCount() >= 1) {
-        $medicos = $stmt->fetchAll(PDO::FETCH_OBJ);
+if ($stmt->rowCount() >= 1) {
+  $medicos = $stmt->fetchAll(PDO::FETCH_OBJ);
 
-        foreach($medicos as $medico) {
-            $agendamentos = json_decode($medico->calendario, true);
-            $excludes = [];
+  foreach ($medicos as $medico) {
+    $agendamentos = json_decode($medico->calendario, true);
+    $excludes = [];
 
-            $gCalendar= new GoogleCalendarSync();
+    $gCalendar = new GoogleCalendarSync();
 
-            $gCalendar->setLink($medico->google_agenda_link, $medico->inicio_ag, $medico->fim_ag, $medico->duracao_atendimento);
-            $gCalendar->fetch();
-            $events = $gCalendar->parse();
-            $events = $gCalendar->getEvents();
+    $gCalendar->setLink($medico->google_agenda_link, $medico->inicio_ag, $medico->fim_ag, $medico->duracao_atendimento);
+    $gCalendar->fetch();
+    $events = $gCalendar->parse();
+    $events = $gCalendar->getEvents();
 
-            $dates = [];
+    $dates = [];
 
-            foreach($events as $event) {
-              foreach($event['DAYS'] as $day => $hours) {
-                if(date('H:i', strtotime($event['DTSTART'])) != '00:00' && date('H:i', strtotime($event['DTEND'])) != '00:00') {
-                  $dates[date('Y-m-d', strtotime($event['DTSTART']))] = [
-                    'start' => date('H:i', strtotime($event['DTSTART'])),
-                    'end' => date('H:i', strtotime($event['DTEND']))
-                  ];
-                }
-              }
-            }
-
-            
-
-            foreach($agendamentos as $date => $item) {
-              if(isset($dates[$date])) {
-                foreach($item as $h => $hrs) {
-                  $startTime = date('Y-m-d H:i', strtotime($event['DTSTART']));
-                  $endTime = date('Y-m-d H:i', strtotime($event['DTEND']));
-                  $checkTime = $date.' '.$h;
-
-                  $check = new DateTime($checkTime);
-                  $start = new DateTime($startTime);
-                  $end = new DateTime($endTime);
-
-                  if ($check >= $start && $check <= $end) {
-                    unset($agendamentos[$date][$h]);
-                  }
-                }
-              }
-            }
-
-            try {
-
-              $calendario = json_encode($agendamentos, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
-              $pdo->query("UPDATE AGENDA_MEDICA SET calendario = '{$calendario}' WHERE medico_token = '{$medico->token}'");
-            }
-            catch(Exception $ex) {
-                $eventsArray = [
-                    'status' => 'danger',
-                    'text' => 'Falha ao Sincronizar Agenda do Google!',
-                    'icon' => 'danger',
-                    'exception' => $ex->getMessage()
-                ];
-            }
-
-            $results[] = $agendamentos;
+    foreach ($events as $event) {
+      foreach ($event['DAYS'] as $day => $hours) {
+        if (date('H:i', strtotime($event['DTSTART'])) != '00:00' && date('H:i', strtotime($event['DTEND'])) != '00:00') {
+          $dates[date('Y-m-d', strtotime($event['DTSTART']))] = [
+            'start' => date('H:i', strtotime($event['DTSTART'])),
+            'end' => date('H:i', strtotime($event['DTEND']))
+          ];
         }
+      }
     }
 
-//header('Content-Type: application/json; charset=utf-8');
-//echo json_encode($events, JSON_PRETTY_PRINT);
+    foreach ($agendamentos as $date => $item) {
+      if (isset($dates[$date])) {
+        foreach ($item as $h => $hrs) {
+          $startTime = date('Y-m-d H:i', strtotime($event['DTSTART']));
+          $endTime = date('Y-m-d H:i', strtotime($event['DTEND']));
+          $checkTime = $date . ' ' . $h;
+
+          $check = new DateTime($checkTime);
+          $start = new DateTime($startTime);
+          $end = new DateTime($endTime);
+
+          if ($check >= $start && $check <= $end) {
+            unset($agendamentos[$date][$h]);
+          }
+        }
+      }
+    }
+
+    try {
+      $calendario = json_encode($agendamentos, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+      $pdo->query("UPDATE AGENDA_MEDICA SET calendario = '{$calendario}' WHERE medico_token = '{$medico->token}'");
+    } catch (Exception $ex) {
+      $eventsArray = [
+        'status' => 'danger',
+        'text' => 'Falha ao Sincronizar Agenda do Google!',
+        'icon' => 'danger',
+        'exception' => $ex->getMessage()
+      ];
+    }
+
+    $results[] = $agendamentos;
+  }
+} else {
+  $eventsArray = [
+    'status' => 'danger',
+    'text' => 'Nenhum médico encontrado!',
+    'icon' => 'danger'
+  ];
+}
+
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($eventsArray, JSON_PRETTY_PRINT);
